fc7a08d8f19fa1ca281149431dbe8e724f919d03
michelvasconcelos <michelvasconcelos@hotmail.com>
2022-06-28 21:58:47 -0300

    Observer e service elevator adicionados

--- concreteObserver.py
+++ concreteObserver.py
@@ -1,5 +1,5 @@
 from observer import Observer
-from concreteSubject import ConcreteSubject
+#from concreteSubject import ConcreteSubject
 
 class MessageObserverOne(Observer):
     def update(self, elev, message, people):
@@ -8,37 +8,37 @@ class MessageObserverOne(Observer):
 
 class MessageObserverTwo(Observer):
     def update(self, elev, message, people):
-        if message == 'maintenance':
+        if message == 'idle':
             if elev == 'social':
                 print(f'Speedup service elevator.')
-            elif elev == 'Service':
+            elif elev == 'service':
                 print(f'Speedup social elevator.')
 
 class MessageObserverThree(Observer):
     def update(self, elev, message, people):
         if message == 'stuck' and people == True:
             print(f'Message to passangers: maintenance and security teams are on their way.')
-            print(f'Start playing loung music on {elev} elevator.')
+            print(f'Start playing lounge music on {elev} elevator.')
 
 class MessageObserverFourth(Observer):
     def update(self, elev, message, people):
-        if message == 'normal':
+        if message == 'up' or message == 'down' or message == 'stop':
             print(f'The {elev} elevator is working properly.')
 
-def main():
-    subject = ConcreteSubject()
-
-    message_one = MessageObserverOne()
-    message_two = MessageObserverTwo()
-    message_three = MessageObserverThree()
-    message_Fourth = MessageObserverFourth()
-
-    subject.attach(message_one)
-    subject.attach(message_two)
-    subject.attach(message_three)
-    subject.attach(message_Fourth)
-
-    subject.notify('social', 'stuck', True)
-
-if __name__ == '__main__':
-    main()
+#def main():
+#    subject = ConcreteSubject()
+#
+#    message_one = MessageObserverOne()
+#    message_two = MessageObserverTwo()
+#    message_three = MessageObserverThree()
+#    message_Fourth = MessageObserverFourth()
+#
+#    subject.attach(message_one)
+#    subject.attach(message_two)
+#    subject.attach(message_three)
+#    subject.attach(message_Fourth)
+#
+#    subject.notify('social', 'stuck', True)
+#
+#if __name__ == '__main__':
+#    main()
--- factory.py
+++ factory.py
@@ -5,6 +5,8 @@ from elevDown import ElevDown
 from elevStop import ElevStop
 from elevStuck import ElevStuck
 from elevIdle import ElevIdle
+from concreteSubject import ConcreteSubject
+from concreteObserver import MessageObserverOne, MessageObserverThree, MessageObserverTwo, MessageObserverFourth
 
 class ElevFactory(ABC):
     @abstractmethod
@@ -22,6 +24,12 @@ class SocialElevFactory(ElevFactory):
     def createElev(self):
         return SocialElevator(self.state_option)
 
+class ServiceElevFactory(ElevFactory):
+    def __init__(self, state_option):
+        self.state_option = state_option
+    def createElev(self):
+        return ServiceElevator(self.state_option)
+
 class IElevators(ABC):
     @abstractmethod
     def move(self):
@@ -30,33 +38,101 @@ class IElevators(ABC):
 class SocialElevator(IElevators):
     def __init__(self, state_option):
         self.state_option = state_option
-        #self.category = 'social'
 
     def move(self):
-        #result = (f'{self.category} elevator is {self.state_option}')
+        
+        subject = ConcreteSubject()
         if self.state_option == 'stuck':
-            res = Elevator(ElevStuck())
-            res.elevator() #result = res.elevator()
+            result = Elevator(ElevStuck())
+            result.elevator()
+            message_one = MessageObserverOne()
+            message_three = MessageObserverThree()
+            subject.attach(message_one)
+            subject.attach(message_three)
+            subject.notify(el, st, fe)
         elif self.state_option == 'up':
-            res = Elevator(ElevUp())
-            res.elevator()
+            result = Elevator(ElevUp())
+            result.elevator()
+            message_Fourth = MessageObserverFourth()
+            subject.attach(message_Fourth)
+            subject.notify(el, st, fe)
         elif self.state_option == 'down':
-            res = Elevator(ElevDown())
-            res.elevator()
+            result = Elevator(ElevDown())
+            result.elevator()
+            message_Fourth = MessageObserverFourth()
+            subject.attach(message_Fourth)
+            subject.notify(el, st, fe)
         elif self.state_option == 'stop':
-            res = Elevator(ElevStop())
-            res.elevator()
+            result = Elevator(ElevStop())
+            result.elevator()
+            message_Fourth = MessageObserverFourth()
+            subject.attach(message_Fourth)
+            subject.notify(el, st, fe)
         elif self.state_option == 'idle':
-            res = Elevator(ElevIdle())
-            res.elevator()
-        #return result
+            result = Elevator(ElevIdle())
+            result.elevator()
+            message_two = MessageObserverTwo()
+            subject.attach(message_two)
+            #If idle, elevator is considered empty
+            subject.notify(el, st, False)
+
+class ServiceElevator(IElevators):
+    def __init__(self, state_option):
+        self.state_option = state_option
+
+    def move(self):
+        
+        subject = ConcreteSubject()
+        if self.state_option == 'stuck':
+            result = Elevator(ElevStuck())
+            result.elevator()
+            message_one = MessageObserverOne()
+            message_three = MessageObserverThree()
+            subject.attach(message_one)
+            subject.attach(message_three)
+            subject.notify(el, st, fe)
+        elif self.state_option == 'up':
+            result = Elevator(ElevUp())
+            result.elevator()
+            message_Fourth = MessageObserverFourth()
+            subject.attach(message_Fourth)
+            subject.notify(el, st, fe)
+        elif self.state_option == 'down':
+            result = Elevator(ElevDown())
+            result.elevator()
+            message_Fourth = MessageObserverFourth()
+            subject.attach(message_Fourth)
+            subject.notify(el, st, fe)
+        elif self.state_option == 'stop':
+            result = Elevator(ElevStop())
+            result.elevator()
+            message_Fourth = MessageObserverFourth()
+            subject.attach(message_Fourth)
+            subject.notify(el, st, fe)
+        elif self.state_option == 'idle':
+            result = Elevator(ElevIdle())
+            result.elevator()
+            message_two = MessageObserverTwo()
+            subject.attach(message_two)
+            #If idle, elevator is considered empty
+            subject.notify(el, st, False)        
 
 def client_code(elev: ElevFactory):
     elev.planMove()
 
 if __name__ == '__main__':
-    client_code(SocialElevFactory('stuck'))
-    client_code(SocialElevFactory('up'))
-    client_code(SocialElevFactory('down'))
-    client_code(SocialElevFactory('stop'))
-    client_code(SocialElevFactory('idle'))
+    
+    el = input('social or service: ').lower()
+    st = input('Elevator state (stuck, up, down, stop or idle):').lower()
+    fe = input('Full or empty (True or False): ')
+    if fe == 'True' or fe == 'true':
+        fe = bool(fe)
+    else:
+        fe = bool('')
+
+    print(fe)
+    print('')
+    if el == 'social':
+        client_code(SocialElevFactory(st))
+    elif el == 'service':
+        client_code(ServiceElevFactory(st))
